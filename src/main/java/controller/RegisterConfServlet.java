0d6762de7cded3438f60d88712d5cf00e9dd0019
shota <shoushou3509@outlook.jp>
2022-07-20 15:10:42 +0900

    登録画面のRegisterConServlet.javaを作成しました。

--- src/main/java/controller/RegisterConfServlet.java
+++ src/main/java/controller/RegisterConfServlet.java
@@ -7,6 +7,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import domain.Member;
 
 /**
  * Servlet implementation class RegisterConfServlet
@@ -21,15 +24,38 @@ public class RegisterConfServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//セッションに登録内容が未登録の場合、登録画面へリダイレクト
+		HttpSession session = request.getSession();
+		if(session.getAttribute("member") == null) {
+			response.sendRedirect("register");
+			return;
+		}
 		
+		//確認画面の表示
+		request.getRequestDispatcher("/WEB-INF/view/register.jsp")
+				.forward(request, response);
 	}
+	
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		//セッションから登録内容を取り出す
+		HttpSession session = request.getSession();
+		Member member = (Member) session.getAttribute("member");
+		
+		//最終的に登録内容を保存する
+		System.out.println("以下の内容を登録しました");
+		System.out.println("名前:" + member.getName());
+		System.out.println("年齢:" + member.getAge());
+		System.out.println("メール:" + member.getEmail());
+		
+		//セッションの内容が不要になったので破棄する
+		session.invalidate();
+		
+		//完了ページへ移動
+		response.sendRedirect("registerDone");
 	}
 
 }
